92f9367438bd038f40104f432fb3358ab935673a
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2024-11-14 19:18:40 +0600

    Update uva_summation_of_four_primes(10168).cpp

--- uva_summation_of_four_primes(10168).cpp
+++ uva_summation_of_four_primes(10168).cpp
@@ -1,4 +1,98 @@
-//uva 10168...
+//nov 11 ,2024
+//uva 10168 Summation of Four Primes.cpp
+#include<bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+//ll prime[260005], nPrime;
+bool mark[20000005];
+vector<long long> v;
+//map<int,pair<int, int>> vis;
+vector<pair<long long,long long>> vis;
+
+void sieve(ll n)
+{
+    int i, j, limit = sqrt(n * 1.) + 2;
+    mark[1] = 1; //we are inserting true value when it is not prime.
+    v.push_back(2);
+    for(int i = 4; i <= n; i += 2)
+    {
+        // arr[i] += 2;
+        mark[i] = 1;
+    }
+    //prime[nPrime++] = 2;
+    for(i = 3; i <= n; i++)
+    {
+        if(mark[i] == 0)
+        {
+            v.push_back(i);
+            //prime[nPrime++] = i;
+            if (i <= limit)
+            {
+                for(j = i * i; j <= n; j+= i * 2)
+                {
+                    mark[j] = 1;
+
+                }
+            }
+
+
+        }
+    }
+}
+int main()
+{
+    ll a;
+    sieve( 10000000);
+    while(cin>>a)
+    {
+        if(a<8)
+        {
+            cout<<"Impossible."<<endl;
+        }
+        else if(a%2==0)
+        {
+            a-=4;
+            //ekta even number k always 2 ta prime number e convert kora jay..must jay..
+            for(ll i=0; i<a; i++)
+            {
+                if( v[i] && (!mark[a-v[i]]) )
+                {
+                    cout<<"2 2 "<<v[i]<<" "<<a-v[i]<<endl;
+                    break;
+                }
+            }
+        }
+        else if(a%2!=0)
+        {
+            a-=5;
+            for(ll i=0; i<a; i++)
+            {
+                if( v[i] && (!mark[a-v[i]]) )
+                {
+                    cout<<"2 3 "<<v[i]<<" "<<a-v[i]<<endl;
+                    break;
+                }
+            }
+
+        }
+    }
+
+}
+/*
+Sample Input
+24
+36
+46
+7
+Sample Output
+3 11 3 7
+3 7 13 13
+11 11 17 7
+Impossible.
+*/
+
+//............................................................................................................
+//uva 10168...done 2 years ago
 #include<bits/stdc++.h>
 #include<algorithm>
 #include<iostream>
